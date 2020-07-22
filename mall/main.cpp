5566405fb5f72aa27f3c8900f486f5126057df32
amandaizumi <68003757+amandaizumi@users.noreply.github.com>
2020-07-22 07:55:20 +0700

    final exam 2

--- mall/main.cpp
+++ mall/main.cpp
@@ -243,17 +243,16 @@ static void resize(int width, int height)
 
 void setupLights()
 {
-	GLfloat ambient[] = {0.7f, 0.7f, 0.7, 1.0f};
-	GLfloat diffuse[] = {0.6f, 0.6f, 0.6, 1.0f};
-	GLfloat specular[] = {1.0f, 1.0f, 1.0, 1.0f};
-	GLfloat shininess[] = {50};
+	GLfloat ambient[] = {0.7f, 0.7f, 0.7, 1.0f}; //cahaya tdk dapat ditebtukn (segala arah)
+	GLfloat diffuse[] = {0.6f, 0.6f, 0.6, 1.0f}; //cahaya bersumber sari 1 arah
+	GLfloat specular[] = {0.4f, 0.4f, 0.4, 0.4f}; // cahaya yang memantul
+	GLfloat shininess[] = {50}; //kesan mengkilap
 	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient);
 	glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuse);
 	glMaterialfv(GL_FRONT, GL_SPECULAR, specular);
 	glMaterialfv(GL_FRONT, GL_SHININESS, shininess);
 
 	GLfloat lightIntensity[] = {0.7f, 0.7f, 1, 1.0f};
-	// GLfloat lightPosition[] = { -7.0f, 6.0f,3.0f, 0.0f };
 	glLightfv(GL_LIGHT0, GL_POSITION, lightIntensity);
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightIntensity);
 }
